88002699045682f88b5b1bd9291facd8cfd91b25
GuhaPranav <142341548+GuhaPranav09@users.noreply.github.com>
2025-07-22 03:25:23 +0530

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -258,10 +258,13 @@ export default function Projects() {
                   <Github className="w-5 h-5 mr-2" />
                   View All Projects
                 </Button>
-                <Button variant="outline">
-                  <ExternalLink className="w-5 h-5 mr-2" />
-                  Get In Touch
-                </Button>
+                
+                <Link to="/contact">
+                  <Button variant="outline">
+                    <ExternalLink className="w-5 h-5 mr-2" />
+                    Get In Touch
+                  </Button>
+                </Link>
               </div>
             </Card>
           </div>
